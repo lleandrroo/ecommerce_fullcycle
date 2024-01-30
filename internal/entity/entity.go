fa9c8561c8f43a2f182efe1f1de389636bc561ca
lleandrroo <lleandrroo@hotmail.com>
2024-01-30 11:57:48 -0300

    create

---
+++ internal/entity/entity.go
@@ -0,0 +1,35 @@
+package entity
+
+import "github.com/google/uuid"
+
+type Category struct {
+	ID   string
+	Name string
+}
+
+func NewCategory(name string) *Category {
+	return &Category{
+		ID:   uuid.New().String(),
+		Name: name,
+	}
+}
+
+type Product struct {
+	ID          string
+	Name        string
+	Description string
+	CategoryID  string
+	ImageURL    string
+	Price       float64
+}
+
+func NewProduct(name, description, categoryID, imageURL string, price float64) *Product {
+	return &Product{
+		ID:          uuid.New().String(),
+		Name:        name,
+		Description: description,
+		Price:       price,
+		CategoryID:  categoryID,
+		ImageURL:    imageURL,
+	}
+}
---
+++ internal/webserver/category_handler.go
@@ -0,0 +1,38 @@
+package webserver
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/lleandrroo/ecommerce_fullcycle/internal/service"
+)
+
+type WebCategoryHandler struct {
+	CategoryService *service.CategoryService
+}
+
+func NewWebCategoryHandler(categoryService *service.CategoryService) *WebCategoryHandler {
+	return &WebCategoryHandler{CategoryService: categoryService}
+}
+
+func (wch *WebCategoryHandler) GetVategories(w http.ResponseWriter, r *http.Request) {
+	categories, err := wch.CategoryService.GetCategories()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(categories)
+}
+
+func (wch *WebCategoryHandler) GetCategory(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		http.Error(w, "id is required", http.StatusBadRequest)
+		return
+	}
+	category, err := wch.CategoryService.GetCategoryID(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
